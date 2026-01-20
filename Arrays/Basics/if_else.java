a3f1892fd72c1f7da55484045b13a49ceb68ae4b
KashishSirohiwal <kashhverma@gmail.com>
2026-01-20 14:27:27 +0530

    feat(Arrays/Basics): Add Java if-else if-else chain

--- Arrays/Basics/if_else.java
+++ Arrays/Basics/if_else.java
@@ -13,6 +13,10 @@ public class if_else {
         // Method 1: Simple if-else
         System.out.println("--- Method 1: Simple if-else ---");
         method1_simpleIfElse(sc);
+
+        // Method 2: if-else if-else chain
+        System.out.println("\n--- Method 2: if-else if-else Chain ---");
+        method2_ifElseIfChain(sc);
         
         sc.close();
     }
@@ -27,6 +31,26 @@ public class if_else {
             System.out.println(num + " is negative");
         } else {
             System.out.println("Number is zero");
+        }
+    }
+
+    /* METHOD 2: if-else if-else Chain */
+    public static void method2_ifElseIfChain(Scanner sc) {
+        System.out.print("Enter a number (1-5) for grade: ");
+        int grade = sc.nextInt();
+       
+        if (grade == 5) {
+            System.out.println("Grade A - Excellent!");
+        } else if (grade == 4) {
+            System.out.println("Grade B - Good!");
+        } else if (grade == 3) {
+            System.out.println("Grade C - Average");
+        } else if (grade == 2) {
+            System.out.println("Grade D - Below Average");
+        } else if (grade == 1) {
+            System.out.println("Grade F - Failed");
+        } else {
+            System.out.println("Invalid grade");
         }
     }
 }
