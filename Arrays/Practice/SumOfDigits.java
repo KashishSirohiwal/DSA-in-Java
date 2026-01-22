7bb57568403018a6e77de241931ccf8fdd79343d
KashishSirohiwal <kashhverma@gmail.com>
2026-01-22 16:05:02 +0530

    Practice: Implemented Sum of Digits using function

---
+++ Arrays/Practice/SumOfDigits.java
@@ -0,0 +1,22 @@
+package Arrays.Practice;
+import java.util.*;
+
+public class SumOfDigits {
+    public static int sumDigits(int num) { 
+        int sum = 0; int rem;
+        while (num > 0) {
+            rem = num % 10; // To get remainder
+            sum += rem; // Sum of each digit
+            num = num/10; // To decrease digit by 1 from end
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        int result = sumDigits(Math.abs(num)); // Math.abs turns no into positive
+        System.out.println("The sum of " + num + " is : " + result);
+        sc.close();
+    }
+}
