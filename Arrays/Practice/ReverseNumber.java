dc6e7c66bcbad232d728332edbda212429c6b7ed
KashishSirohiwal <kashhverma@gmail.com>
2026-01-22 18:55:36 +0530

    feat(practice): reverse number with negative case handling

--- Arrays/Practice/ReverseNumber.java
+++ Arrays/Practice/ReverseNumber.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class ReverseNumber {
     public static int reverseno(int num) { 
-        int digit; int reverse = 0;
+        int reverse = 0;
+        boolean isNegative = num < 0; // Checks if number is negative
+        num = Math.abs(num); // Turning into positive equivalent
         while (num > 0) {
-            digit = num % 10; // Extract the last digit
+            int digit = num % 10; // Extract the last digit
             reverse = reverse * 10 + digit;
             num /= 10; // Remove last digit
         }
-        return reverse;
+        return isNegative ? -reverse : reverse;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
         int num = sc.nextInt();
-        int result = reverseno(Math.abs(num)); // Math.abs turns no into positive
+        int result = reverseno(num); // Math.abs turns no into positive
         System.out.println("The reverse of " + num + " is : " + result);
         sc.close();
     }
